d248e8a86953f28fd10745f8ca963494233160f3
roja <roja19p@gmail.com>
2011-09-09 15:28:26 +0530

    1. Updated anu_compile64bit.sh  2. Updated step-debug  3. Updated Log.

--- bin/step-debug/std-run-lwg-onward.sh
+++ bin/step-debug/std-run-lwg-onward.sh
@@ -12,25 +12,20 @@
 
 sed -e 's/#//g' $MYPATH/$1_tmp/$2/id_Apertium_output1.dat > $MYPATH/$1_tmp/$2/id_Apertium_output.dat
 
-cd $MYPATH/$1_tmp/$2
+ cd $MYPATH/$1_tmp/$2
+ cp $MYPATH/$1_tmp/underscore_hyphen_replace_info.txt  $MYPATH/$1_tmp/$2/underscore_hyphen_replace_info.dat
  myclips -f $HOME_anu_test/Anu_clp_files/run_H_gen_sen.bat >> $1.error
  
  cat  para_sent_id_info.dat original_word.dat word.dat punctuation_info.dat chunk.dat cat_consistency_check.dat padasuthra.dat root.dat  revised_preferred_morph.dat parserid_wordid_mapping.dat lwg_info.dat relations.dat hindi_meanings.dat GNP_agmt_info.dat id_Apertium_output.dat  hindi_id_reorder.dat English_sentence.dat hindi_sentence.dat >>$MYPATH/$1_tmp/$2/all_facts
 
-
- cp hindi_sentence.dat hindi_sentence_tmp.dat
-
  sh $HOME_anu_test/bin/abbr.sh
-
 
  cp hindi_sentence1.dat  hindi_sentence.dat
 
  cat  hindi_sentence.dat
  
-
  grep -B2 "FALSE" $1.error >> errors.txt
  cat errors.txt
-
 
  #for sentence by sent analysis for web debugging tutorial
  cat English_sentence.dat >> $MYPATH/$1_tmp/sent-by-sent
@@ -41,3 +36,5 @@ cd $MYPATH/$1_tmp/$2
  myclips -f $HOME_anu_test/Anu_clp_files/user_wsd_info.clp > /dev/null
  mv user_wsd_info.dat $MYPATH/$1_$2_user_wsd_info.dat
 
+ sed  's/LB /(/g' $MYPATH/$1_tmp/$2/rev_constituency_tree.dat |sed 's/RB /)/g' |sed 's/RB)$/))/g'> $MYPATH/$1_tmp/$2/rev_constituency_tree1.dat
+cat $MYPATH/$1_tmp/$2/rev_constituency_tree1.dat >>$MYPATH/$1_tmp/rev_constituency_tree2.dat
