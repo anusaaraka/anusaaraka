396eb62cab853f622a1058a6d29e6417a6ef0947
mahalaxmi <mahalaxmi@anu.(none)>
2012-01-20 16:24:05 +0530

    Added new files english_hindi_tran.css english_hindi_tran.js files

---
+++ Browser/src/english_hindi_tran.js
@@ -0,0 +1,21 @@
+/*
+Configuration file for English-Hinid language specifics
+*/
+
+var rows_visibility = [
+1, // row1
+1, // row2
+0, // row3
+]; // The array can be of different size than the number of rows at hand
+
+var row_labels = [
+"Hindi  (A)",
+"English  (B)",
+"Suggestions (C)"
+];
+
+var row_descriptions = [
+"Shows Hindi Generation ",
+"Original English text",
+"Shows the user suggestion"
+];
