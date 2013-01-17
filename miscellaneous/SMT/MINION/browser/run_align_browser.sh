697f6ef48b23cc30c3312722f962f4cbe026cada
manjusiri <sirimanju@gmail.com>
2013-01-17 13:43:09 +0530

    Modified path in run_align_browser.sh and Updated Log

--- miscellaneous/SMT/MINION/browser/run_align_browser.sh
+++ miscellaneous/SMT/MINION/browser/run_align_browser.sh
@@ -1,5 +1,5 @@
 #/bin/sh
- timeout 300 myclips -f $HOME_anu_test/miscellaneous/SMT/alignment/browser/run_hin_align_html.bat > $3/tmp/$2_tmp/xml.err
+ timeout 300 myclips -f $HOME_anu_test/miscellaneous/SMT/MINION/browser/run_hin_align_html.bat > $3/tmp/$2_tmp/xml.err
 
  sh $HOME_anu_test/bin/replace-abbrevations.sh $3/tmp/$2_tmp/$2_align_tmp.html $3/tmp/$2_tmp/$2_align_tmp1.html  
 
@@ -16,4 +16,4 @@
 
  sh $3/tmp/$2_tmp/proper_nouns_utf8.sh $3/tmp/$2_tmp/$2_align_tmp4.html > $4/$2_align.html
  
- cp $HOME_anu_test/miscellaneous/SMT/alignment/browser/hindi_alignment.css $HOME_anu_test/miscellaneous/SMT/alignment/browser/hindi_alignment.js $4
+ cp $HOME_anu_test/miscellaneous/SMT/MINION/browser/hindi_alignment.css $HOME_anu_test/miscellaneous/SMT/MINION/browser/hindi_alignment.js $4
