d72481c5c34bf0acb055db590d7370dd967ad2a3
roja <roja19p@gmail.com>
2013-08-31 18:13:33 +0530

    1. Modified following tregex files: 	std_tregex/README 	run_tregex.sh 2. Added run_tregex_for_multiparse.sh to view multiple parse. 3. Updated Log.

--- miscellaneous/std_tregex/run_tregex.sh
+++ miscellaneous/std_tregex/run_tregex.sh
@@ -1,4 +1,2 @@
-#$HOME_anu_test/Parsers/stanford-parser/stanford-parser-full-2013-06-20/run_multiple_parse_penn.sh $HOME_anu_test/miscellaneous/std_tregex/$1 > $HOME_anu_test/miscellaneous/std_tregex/$1_output
 $HOME_anu_test/Parsers/stanford-parser/stanford-parser-full-2013-06-20/run_penn-pcfg.sh $HOME_anu_test/miscellaneous/std_tregex/$1 > $HOME_anu_test/miscellaneous/std_tregex/$1_output
-#sed -i '1,1d' $HOME_anu_test/miscellaneous/std_tregex/$1_output
 sh $HOME_anu_test/miscellaneous/std_tregex/stanford-tregex-2013-06-20/run-tregex-gui.command $HOME_anu_test/miscellaneous/std_tregex/$1_output
