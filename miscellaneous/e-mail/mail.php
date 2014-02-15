0edce66fd7bb79362f20403598f7f1b2d5905124
roja <roja19p@gmail.com>
2014-02-15 16:48:56 +0530

    Updated mail.php(to CC emails) and also modified mail.js

--- miscellaneous/e-mail/mail.php
+++ miscellaneous/e-mail/mail.php
@@ -3,6 +3,7 @@
 
 <?php
 $to = $_GET['email'];
+$cc = $_GET['Cc'];
 $subject = $_GET['subject'];
 $message = $_GET['message'];
 $message = str_replace( "Root" , "\nRoot" , $message );
@@ -12,6 +13,7 @@ $message = str_replace( "Manual" , "\nManual" , $message );
 $message = str_replace( "Suggestion" , "\nSuggestion" , $message );
 $from = "root@localhost";
 $headers = "From:" . $from;
+$headers = "Cc:" .$cc. "\n";
 $ret = mail($to,$subject,$message,$headers);
 ?>
 
