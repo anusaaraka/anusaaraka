77a2837960a6e403b3c2ab8afe1959d50edb9958
roja <roja19p@gmail.com>
2014-06-27 13:50:57 +0530

    Added replace-mapping-symbols_for_align.lex and Updated Anusaaraka_alignment.sh Readme compile.sh run_alignment.sh

--- miscellaneous/SMT/phrasal_alignment/compile.sh
+++ miscellaneous/SMT/phrasal_alignment/compile.sh
@@ -1,5 +1,6 @@
 gcc -o gdbm-fetch.out gdbm-fetch.c -lgdbm
 $HOME_anu_test/Anu_src/comp.sh replace-punctuation
+$HOME_anu_test/Anu_src/comp.sh replace-mapping-symbols_for_align
 $HOME_anu_test/Anu_data/create-gdbm.pl  en-hi-gdbm-dict.gdbm < en-hi-gdbm-dict.txt
 $HOME_anu_test/Anu_src/comp.sh $HOME_anu_test/miscellaneous/SMT/MINION/alignment/morph
 cd hi-en
--- miscellaneous/SMT/phrasal_alignment/run_alignment.sh
+++ miscellaneous/SMT/phrasal_alignment/run_alignment.sh
@@ -10,11 +10,8 @@ python count.py $1/shortest-path-value.dat $1/count_dict.dat  $1/count_dict_with
 sh sort.sh $1/count_dict_with_length.dat $1/sorted_file.dat  2>> $1/phrasal_error
 python get_phrase.py  $1/hnd  $1/sorted_file.dat $1/shortest-path-value.dat $1/left-over-words.dat > $1/align_eng.dat 2>> $1/phrasal_error
 python map-eng-offset.py  $1/align_eng.dat  $1/map.dat > $1/mapped.dat 2>> $1/phrasal_error
-cd $1
-sh $MYPATH/sriram/sriram.sh match-value.dat
-cd $MYPATH
 echo "Word alignment" >> $1/phrasal_error
-python get_word_align.py Word-to-Word-dict.txt   $1/mapped-new.dat $1/hnd $1/left > $1/wrd-to-wrd.txt1 2>> $1/phrasal_error
+python get_word_align.py Word-to-Word-dict.txt   $1/mapped.dat $1/hnd $1/left > $1/wrd-to-wrd.txt1 2>> $1/phrasal_error
 echo "Aligning left over words" >> $1/phrasal_error  2>> $1/phrasal_error
 python align-left-over-wrds.py  $1/wrd-to-wrd.txt1  $1/hnd  $1/left $1/left-over-words.dat $1/align_left_over_wrds.dat > $1/wrd-to-wrd.dat 2>> $1/phrasal_error
 python mapping.py $1/wrd-to-wrd.dat > $1/mapped1.dat 2>> $1/phrasal_error
