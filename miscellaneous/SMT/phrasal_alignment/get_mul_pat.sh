37c1ebf42a9a4f40c3c85ed809b7795199765771
roja <roja19p@gmail.com>
2014-07-04 19:20:39 +0530

    Updated get_mul_path.sh and multifast/src/run.sh

--- miscellaneous/SMT/phrasal_alignment/get_mul_pat.sh
+++ miscellaneous/SMT/phrasal_alignment/get_mul_pat.sh
@@ -1,7 +1,7 @@
 cut -f2 -d' ' $1 > tmp_1
 sort -u tmp_1 > tmp_2 
 sed 's/ \[/\t[/'  tmp_2 | sed 's/ /##/g' | sed 's/__/_/g'  | sed 's/^_//g' | sed 's/_$//g' | sed 's/"/\\"/g' > tmp
-$HOME_anu_test/multifast-v1.0.0/src/get_word_count.out  tmp > tmp1
+$HOME_anu_test/multifast-v1.0.0/src/get_word_count_single.out  tmp > tmp1
 sed 's/##//g' tmp1 | sed 's/\t/_",0, {stringy: "/g' | sed 's/^/\t{"_/g' | sed 's/$/"}},/g' > tmp2
 sed '1i\AC_PATTERN_t allpattern[] = {'  tmp2 > tmp3
 sed '$a\};'   tmp3  > $2
