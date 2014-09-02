30fd406a18803c775da07ecf8005c65fa9a4bb1f
roja <roja19p@gmail.com>
2014-09-02 12:23:23 +0530

    Updated tregex README, run_tregex.sh run_tregex_for_multiparse.sh

--- miscellaneous/std_tregex/run_tregex.sh
+++ miscellaneous/std_tregex/run_tregex.sh
@@ -1,2 +1,6 @@
-$HOME_anu_test/Parsers/stanford-parser/stanford-parser-full-2014-01-04/run_penn-pcfg.sh $HOME_anu_test/miscellaneous/std_tregex/$1 > $HOME_anu_test/miscellaneous/std_tregex/$1_output
-sh $HOME_anu_test/miscellaneous/std_tregex/stanford-tregex-2013-06-20/run-tregex-gui.command $HOME_anu_test/miscellaneous/std_tregex/$1_output
+STANFORD_PATH=$HOME_anu_test/Parsers/stanford-parser/stanford-parser-full-2014-08-27
+MYPATH=`pwd`
+cd $HOME_anu_test/Parsers/stanford-parser/src
+sh run_penn-pcfg.sh $STANFORD_PATH $HOME_anu_test/miscellaneous/std_tregex/$1 > $HOME_anu_test/miscellaneous/std_tregex/$1_output
+cd $MYPATH
+sh $HOME_anu_test/miscellaneous/std_tregex/stanford-tregex-2014-08-27/run-tregex-gui.command $HOME_anu_test/miscellaneous/std_tregex/$1_output
--- miscellaneous/std_tregex/run_tregex_for_multiparse.sh
+++ miscellaneous/std_tregex/run_tregex_for_multiparse.sh
@@ -1,3 +1,7 @@
-$HOME_anu_test/Parsers/stanford-parser/stanford-parser-full-2014-01-04/run_multiple_parse_penn.sh $1 > $1_output
-sed -i '1,1d' $1_output
-sh $HOME_anu_test/miscellaneous/std_tregex/stanford-tregex-2014-01-04/run-tregex-gui.command $1_output
+STANFORD_PATH=$HOME_anu_test/Parsers/stanford-parser/stanford-parser-full-2014-08-27
+MYPATH=`pwd`
+cd $HOME_anu_test/Parsers/stanford-parser/src
+sh run_multiple_parse_penn.sh $STANFORD_PATH $MYPATH/$1 > $MYPATH/$1_output
+sed -i '1,1d' $MYPATH/$1_output
+cd $MYPATH
+sh $HOME_anu_test/miscellaneous/std_tregex/stanford-tregex-2014-08-27/run-tregex-gui.command $1_output
