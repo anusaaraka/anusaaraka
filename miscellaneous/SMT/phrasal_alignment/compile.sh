2256c6e987a2617d4d913c06bf89173bc834ec2a
roja <roja19p@gmail.com>
2014-09-11 15:52:46 +0530

    Updated compile.sh

--- miscellaneous/SMT/phrasal_alignment/compile.sh
+++ miscellaneous/SMT/phrasal_alignment/compile.sh
@@ -1,7 +1,7 @@
 gcc -o gdbm-fetch.out gdbm-fetch.c -lgdbm
 $HOME_anu_test/Anu_src/comp.sh replace-punctuation
 $HOME_anu_test/Anu_data/create-gdbm.pl  en-hi-gdbm-dict.gdbm < en-hi-gdbm-dict.txt
-$HOME_anu_test/Anu_data/create-gdbm.pl  en-hi-gdbm-dict_health.gdbm < en-hi-gdbm-dict_health.txt
+#$HOME_anu_test/Anu_data/create-gdbm.pl  en-hi-gdbm-dict_health.gdbm < en-hi-gdbm-dict_health.txt
 $HOME_anu_test/Anu_src/comp.sh $HOME_anu_test/miscellaneous/SMT/MINION/alignment/morph
 
 sh get_rev_alltam.sh $HOME_anu_test/Anu_data/canonical_form_dictionary/dictionaries/AllTam_in_canonical_form.txt   $HOME_anu_test/Anu_data/canonical_form_dictionary/dictionaries/AllTam_rev_in_canonical_form.txt
