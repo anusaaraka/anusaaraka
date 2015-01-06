6db0b30cb4e07de89c4407b39076c74b18484a67
roja <roja19p@gmail.com>
2015-01-06 17:15:26 +0530

    1. Separated alignment related mwe's in phrasal_mwe folder 2. Added  phrasal_mwe folder: 	extract_hindi_key.c 	extract_key_using_multifast.c 	extract_key_using_multifast-hi-en-hi.c 	extract_key_in_hindi_phrase.c 	extract_key_using_multifast-hi-en.c 	Makefile 3. Updated 'phrasal_mwe' path for the below files in miscellaneous/SMT/phrasal_alignment: 	compile.sh 	run_alignment.sh 	run_alignment-hi-en.sh 4. Updated Log

--- miscellaneous/SMT/phrasal_alignment/compile.sh
+++ miscellaneous/SMT/phrasal_alignment/compile.sh
@@ -2,6 +2,7 @@ gcc -o gdbm-fetch.out gdbm-fetch.c -lgdbm
 $HOME_anu_test/Anu_src/comp.sh replace-punctuation
 $HOME_anu_test/Anu_data/create-gdbm.pl  en-hi-gdbm-dict.gdbm < en-hi-gdbm-dict.txt
 #$HOME_anu_test/Anu_data/create-gdbm.pl  en-hi-gdbm-dict_health.gdbm < en-hi-gdbm-dict_health.txt
+#$HOME_anu_test/Anu_data/create-gdbm.pl  en-hi-gdbm-dict_tourism.gdbm < en-hi-gdbm-dict_tourism.txt
 $HOME_anu_test/Anu_src/comp.sh $HOME_anu_test/miscellaneous/SMT/MINION/alignment/morph
 
 sh get_rev_alltam.sh $HOME_anu_test/Anu_data/canonical_form_dictionary/dictionaries/AllTam_in_canonical_form.txt   $HOME_anu_test/Anu_data/canonical_form_dictionary/dictionaries/AllTam_rev_in_canonical_form.txt
@@ -9,10 +10,8 @@ $HOME_anu_test/Anu_data/create-gdbm.pl  $HOME_anu_test/Anu_databases/AllTam_rev.
 
 cd hi-en
 $HOME_anu_test/Anu_data/create-gdbm.pl  Hin-Eng-dic.gdbm < Hin-Eng-dic.txt
+#$HOME_anu_test/Anu_data/create-gdbm.pl  Hin-Eng-dic_tourism.gdbm < Hin-Eng-dic_tourism.txt
 #$HOME_anu_test/Anu_data/create-gdbm.pl  Hin-Eng-dic-hi.gdbm < Hin-Eng-dic.txt-hi-en
 
-cd $HOME_anu_test/multifast-v1.0.0/src/
-if ! [ -e extract_hindi_key.c ] ; then 
-	touch extract_hindi_key.c
-fi
+cd $HOME_anu_test/multifast-v1.0.0/src/phrasal_mwe
 make
